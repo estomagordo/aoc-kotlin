9e95d283f105a6a0d9ceaed01dace2e914c2c893
Christofer Ohlsson <christofer@miro.com>
2023-11-07 12:43:27 +0100

    very ugly, possibly not even generally correct

--- solutions/aockt/y2015/Y2015D08.kt
+++ solutions/aockt/y2015/Y2015D08.kt
@@ -5,7 +5,14 @@ import aockt.helpers.lines
 import io.github.jadarma.aockt.core.Solution
 
 object Y2015D08 : Solution {
-    private fun countDoubleQuotesAndHexes(s: String): Pair<Int, Int> {
+    data class Escapes(val doubleQuotes: Int,
+                       val hexEscapes: Int,
+                       val backSlashes: Int,
+                       val tripleBackslashes: Int,
+                       val quadrupleBackslashes: Int
+    )
+
+    private fun countEscapes(s: String): Escapes {
         val doubleQuotes = s.count { it == '"' }
         val hexEscapes = (3..<s.length).count {
             s[it].isHexadecimal() &&
@@ -13,18 +20,34 @@ object Y2015D08 : Solution {
                 s[it - 2] == 'x' &&
                 s[it - 3] == '\\'
         }
-        return Pair(doubleQuotes, hexEscapes)
+        val backslashes = (1..<s.length).count {
+            s[it] == '\\' &&
+                s[it - 1] == '\\'
+        }
+        val tripleBackslashes = (2..<s.length).count {
+            s[it] == '\\' &&
+                    s[it - 1] == '\\' &&
+                    s[it - 2] == '\\'
+        }
+        val quadrupleBackslashes = (2..<s.length).count {
+            s[it] == '\\' &&
+                    s[it - 1] == '\\' &&
+                    s[it - 2] == '\\' &&
+                    s[it - 3] == '\\'
+        }
+
+        return Escapes(doubleQuotes, hexEscapes, backslashes, tripleBackslashes, quadrupleBackslashes)
     }
     private fun countDecodingDifference(s: String): Int {
-        val occurrences = countDoubleQuotesAndHexes(s)
+        val occurrences = countEscapes(s)
 
-        return occurrences.first + occurrences.second * 3
+        return occurrences.doubleQuotes + occurrences.hexEscapes * 3 + occurrences.backSlashes - occurrences.tripleBackslashes + occurrences.quadrupleBackslashes
     }
 
     private fun countEncodingDifference(s: String): Int {
-        val occurrences = countDoubleQuotesAndHexes(s)
+        val occurrences = countEscapes(s)
 
-        return occurrences.first * 2 + occurrences.second
+        return occurrences.doubleQuotes * 2 + occurrences.hexEscapes + occurrences.backSlashes * 2 - occurrences.tripleBackslashes * 2 + occurrences.quadrupleBackslashes * 2
     }
 
     override fun partOne(input: String): Int {
